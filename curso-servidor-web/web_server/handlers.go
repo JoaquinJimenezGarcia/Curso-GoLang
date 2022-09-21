a5ceeed713e2170ee6ba71ce0c7768d77b9252a9
Joaquin <garcia.jjimenez@gmail.com>
2022-09-21 19:08:04 +0200

    Created struct for user and reading/writing json

--- curso-servidor-web/web_server/handlers.go
+++ curso-servidor-web/web_server/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,24 @@ func HandleRoot(writer http.ResponseWriter, r *http.Request) {
 
 func HandleHome(writer http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(writer, "This is the API endpoint")
+}
+
+func UserPostRequest(writer http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(writer, "Error: %v")
+		return
+	}
+
+	response, err := user.ToJson()
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(response)
 }
