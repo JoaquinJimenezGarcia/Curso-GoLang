5f5fab81cb2ca4d45c1d32884e83db027f697d71
Joaquin <garcia.jjimenez@gmail.com>
2022-09-25 15:17:17 +0200

    Add folder and file for cache testing

---
+++ curso-avanzado/cache/main.go
@@ -0,0 +1,37 @@
+package main
+
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+type Memory struct {
+	f     Function
+	cache map[int]FunctionResult
+}
+
+type Function func(key int) (interface{}, error)
+type FunctionResult struct {
+	value interface{}
+	err   error
+}
+
+func NewCache(f Function) *Memory {
+	return &Memory{
+		f:     f,
+		cache: make(map[int]FunctionResult),
+	}
+}
+
+func (m *Memory) Get(key int) (interface{}, error) {
+	result, exists := m.cache[key]
+	if !exists {
+		result.value, result.err = m.f(key)
+		m.cache[key] = result
+	}
+
+	return result.value, result.err
+}
