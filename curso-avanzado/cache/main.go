883f864a869398066b7e85ed20f940cfa5ed5f46
Joaquin <garcia.jjimenez@gmail.com>
2022-09-27 22:46:01 +0200

    Added concurrency and lockers to cache

--- curso-avanzado/cache/main.go
+++ curso-avanzado/cache/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
 func Fibonacci(n int) int {
 	if n <= 1 {
 		return n
@@ -11,6 +18,7 @@ func Fibonacci(n int) int {
 type Memory struct {
 	f     Function
 	cache map[int]FunctionResult
+	lock  sync.Mutex
 }
 
 type Function func(key int) (interface{}, error)
@@ -27,11 +35,44 @@ func NewCache(f Function) *Memory {
 }
 
 func (m *Memory) Get(key int) (interface{}, error) {
+	m.lock.Lock()
 	result, exists := m.cache[key]
+	m.lock.Unlock()
 	if !exists {
+		m.lock.Lock()
 		result.value, result.err = m.f(key)
 		m.cache[key] = result
 	}
 
+	m.lock.Unlock()
+
 	return result.value, result.err
+}
+
+func GetFibonacci(n int) (interface{}, error) {
+	return Fibonacci(n), nil
+}
+
+func main() {
+	cache := NewCache(GetFibonacci)
+	fibo := []int{42, 40, 41, 42, 38}
+	var wg sync.WaitGroup
+
+	for _, n := range fibo {
+		wg.Add(1)
+		go func(index int) {
+			defer wg.Done()
+
+			start := time.Now()
+
+			value, err := cache.Get(index)
+			if err != nil {
+				log.Println(err)
+			}
+
+			fmt.Printf("%d, %s, %d\n", index, time.Since(start), value)
+		}(n)
+	}
+
+	wg.Wait()
 }
