63ffb29e574d7cc8a9c7a3a7b8426e2eb09e69b8
Joaquín Jiménez García <terrorsys@Joaquins-MacBook-Pro.local>
2022-10-02 20:11:25 +0200

    Added routines to port scanner

--- curso-avanzado/net/port.go
+++ curso-avanzado/net/port.go
@@ -3,16 +3,26 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
-	for i := 0; i < 100; i++ {
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
-		if err != nil {
-			continue
-		}
-		conn.Close()
+	var wg sync.WaitGroup
 
-		fmt.Printf("Port %d is open\n", i)
+	for i := 0; i < 65535; i++ {
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", port))
+			if err != nil {
+				return
+			}
+			conn.Close()
+
+			fmt.Printf("Port %d is open\n", port)
+		}(i)
 	}
+
+	wg.Wait()
 }
